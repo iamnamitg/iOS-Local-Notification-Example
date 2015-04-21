18a642a31bc4be3736fc12d28cc5f0ae47c9fd43
namitgupta15 <contact@namitgupta.com>
2015-04-21 18:09:50 +0530

    Added start and stop buttons, repeating notification and alert sound.

--- Local Notification Example/AppDelegate.m
+++ Local Notification Example/AppDelegate.m
@@ -21,11 +21,7 @@
 }
 
 -(void)application:(UIApplication *)application didRegisterUserNotificationSettings:(UIUserNotificationSettings *)notificationSettings{
-    UILocalNotification* localNotification = [[UILocalNotification alloc] init];
-    localNotification.fireDate = [NSDate dateWithTimeIntervalSinceNow:20]; //Enter the time here in seconds.
-    localNotification.alertBody = @"Your alert message";
-    localNotification.timeZone = [NSTimeZone defaultTimeZone];
-    [[UIApplication sharedApplication] scheduleLocalNotification:localNotification];
+ 
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
--- Local Notification Example/ViewController.m
+++ Local Notification Example/ViewController.m
@@ -8,7 +8,14 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController (){
+    NSUserDefaults *defaults;
+}
+
+@property (weak, nonatomic) IBOutlet UILabel *ibLabel1;
+@property (weak, nonatomic) IBOutlet UILabel *ibLabel2;
+@property (weak, nonatomic) IBOutlet UIButton *ibStartButton;
+@property (weak, nonatomic) IBOutlet UIButton *ibStopButton;
 
 @end
 
@@ -17,9 +24,46 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    defaults = [NSUserDefaults standardUserDefaults];
     UIUserNotificationType types = UIUserNotificationTypeBadge | UIUserNotificationTypeSound | UIUserNotificationTypeAlert;
     UIUserNotificationSettings *mySettings = [UIUserNotificationSettings settingsForTypes:types categories:nil];
     [[UIApplication sharedApplication] registerUserNotificationSettings:mySettings];
+}
+
+-(void)viewWillAppear:(BOOL)animated{
+    BOOL notificationIsActive = [defaults boolForKey:@"notificationIsActive"];
+    if (notificationIsActive == true) {
+        self.ibLabel1.hidden = false;
+        self.ibLabel2.hidden = false;
+        self.ibStartButton.hidden = true;
+        self.ibStopButton.hidden = false;
+    }
+}
+
+- (IBAction)ibaStartButton:(id)sender {
+    [defaults setBool:YES forKey:@"notificationIsActive"];
+    [defaults synchronize];
+    self.ibLabel1.hidden = false;
+    self.ibLabel2.hidden = false;
+    self.ibStartButton.hidden = true;
+    self.ibStopButton.hidden = false;
+    UILocalNotification* localNotification = [[UILocalNotification alloc] init];
+    localNotification.fireDate = [NSDate dateWithTimeIntervalSinceNow:0]; //Enter the time here in seconds.
+    localNotification.alertBody = @"Your alert message";
+    localNotification.timeZone = [NSTimeZone defaultTimeZone];
+    localNotification.repeatInterval = NSCalendarUnitMinute; //Repeating instructions here.
+    localNotification.soundName = UILocalNotificationDefaultSoundName;
+    [[UIApplication sharedApplication] scheduleLocalNotification:localNotification];
+}
+
+- (IBAction)ibaStopButton:(id)sender {
+    [defaults setBool:NO forKey:@"notificationIsActive"];
+    [defaults synchronize];
+    self.ibLabel1.hidden = true;
+    self.ibLabel2.hidden = true;
+    self.ibStartButton.hidden = false;
+    self.ibStopButton.hidden = true;
+    [[UIApplication sharedApplication] cancelAllLocalNotifications];
 }
 
 - (void)didReceiveMemoryWarning {
